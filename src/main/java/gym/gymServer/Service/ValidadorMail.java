2ca0c436d56dce4179f8f9016ff24cf356b9734a
GermanDellaNave <gdellanave@correo.um.edu.uy>
2022-10-23 22:23:28 -0300

    Secuencia para validar emails

--- src/main/java/gym/gymServer/Service/ValidadorMail.java
+++ src/main/java/gym/gymServer/Service/ValidadorMail.java
@@ -12,6 +12,8 @@ public class ValidadorMail implements Predicate<String> {
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
+    /*"^[A-Za-z0-9+_.-]+@(.+)$", ^[A-Za-z0-9+_.-]*/
+
     @Override
     public boolean test(String s){
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(s);
