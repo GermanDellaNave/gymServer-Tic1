53ff0de76bbf8c28f8bd3098a4ed67b0c35b60da
mbferreiro <bferreiro@correo.um.edu.uy>
2022-10-29 17:16:14 -0300

    TipoActividad

--- src/main/java/gym/gymServer/Controller/TipoActividadController.java
+++ src/main/java/gym/gymServer/Controller/TipoActividadController.java
@@ -1,4 +1,41 @@
 package gym.gymServer.Controller;
 
+import gym.gymServer.Classes.Actividad;
+import gym.gymServer.Classes.TipoActividad;
+import gym.gymServer.Service.TipoActividadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/tipoactividad")
 public class TipoActividadController {
+
+    @Autowired
+    TipoActividadService tipoActividadService;
+
+    public TipoActividadController() {
+    }
+
+    public TipoActividadController(TipoActividadService tipoActividadService) {
+        this.tipoActividadService = tipoActividadService;
+    }
+
+    @PostMapping
+    public void registrarTipo(@RequestBody TipoActividad tipoActividad){
+        tipoActividadService.registrarTipo(tipoActividad);
+    }
+
+    @GetMapping("/allTipoActividad")
+    public List<TipoActividad> getActividades() {
+        return tipoActividadService.getTiposActividad();
+    }
+
+    @DeleteMapping (path = {"/delete/{tipo}"})
+    public void borrarTipoActividad(@PathVariable("tipo") String tipo {
+        tipoActividadService.borrarTipoActividad(tipo);
+    }
+
+
 }
--- src/main/java/gym/gymServer/Service/TipoActividadService.java
+++ src/main/java/gym/gymServer/Service/TipoActividadService.java
@@ -1,4 +1,38 @@
 package gym.gymServer.Service;
 
+import gym.gymServer.Classes.Actividad;
+import gym.gymServer.Classes.TipoActividad;
+import gym.gymServer.Repository.TipoActividadRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
 public class TipoActividadService {
+
+    TipoActividadRepository tipoActividadRepository;
+
+    public TipoActividadService() {
+    }
+
+    public TipoActividadService(TipoActividadRepository tipoActividadRepository) {
+        this.tipoActividadRepository = tipoActividadRepository;
+    }
+
+    public void registrarTipo(TipoActividad tipoActividad){
+        if(tipoActividadRepository.findOneByTipo(tipoActividad)==null){
+            tipoActividadRepository.save(tipoActividad);
+        }
+    }
+
+    public List<TipoActividad> getTiposActividad() {
+        return (List<TipoActividad>) tipoActividadRepository.findAll();
+    }
+
+    public void borrarTipoActividad(String tipo) {
+        if (tipoActividadRepository.findOneByNombreTipo(tipo)!= null) {
+            tipoActividadRepository.deleteById(tipo);
+    }
 }
