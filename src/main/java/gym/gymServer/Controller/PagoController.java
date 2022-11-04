d6fa35d65e41c690f6d126f6b54c521c108b867f
mbferreiro <bferreiro@correo.um.edu.uy>
2022-11-04 16:26:49 -0300

    holaaa

--- src/main/java/gym/gymServer/Controller/PagoController.java
+++ src/main/java/gym/gymServer/Controller/PagoController.java
@@ -1,7 +1,31 @@
 package gym.gymServer.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import gym.gymServer.Classes.Actividad;
+import gym.gymServer.Classes.Pago;
+import gym.gymServer.Service.InscripcionesActividadesService;
+import gym.gymServer.Service.PagoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequestMapping(path = "api/pagos")
 @RestController
 public class PagoController {
+
+    @Autowired
+    PagoService pagoService;
+
+    public PagoController() {
+    }
+
+    @GetMapping("/allPagos")
+    public List<Pago> getPagos() {
+        return pagoService.getPagos();
+    }
+
+    @PostMapping
+    public void registrarPago(@RequestBody Pago pago) {
+        pagoService.registrarPago(pago);
+    }
 }
---
+++ src/main/java/gym/gymServer/Service/PagoService.java
@@ -0,0 +1,32 @@
+package gym.gymServer.Service;
+
+import gym.gymServer.Classes.Actividad;
+import gym.gymServer.Classes.Pago;
+import gym.gymServer.Repository.PagoRepository;
+import gym.gymServer.Repository.TipoActividadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PagoService {
+
+    @Autowired
+    PagoRepository pagoRepository;
+
+    public PagoService() {
+    }
+
+    public PagoService(PagoRepository pagoRepository) {
+        this.pagoRepository = pagoRepository;
+    }
+
+    public List<Pago> getPagos() {
+        return (List<Pago>) pagoRepository.findAll();
+    }
+
+    public void registrarPago(Pago pago) {
+        pagoRepository.save(pago);
+    }
+}
