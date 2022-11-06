7e96bf10fa98c0c6e8e5ba65484dffa04f538e5f
GermanDellaNave <gdellanave@correo.um.edu.uy>
2022-11-06 19:53:48 -0300

    cobrar

--- src/main/java/gym/gymServer/Repository/PagoRepository.java
+++ src/main/java/gym/gymServer/Repository/PagoRepository.java
@@ -1,8 +1,14 @@
 package gym.gymServer.Repository;
 
+import gym.gymServer.Classes.Empleado;
 import gym.gymServer.Classes.Pago;
 import gym.gymServer.Classes.PagoId;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PagoRepository extends CrudRepository<Pago, PagoId> {
+
+    @Query(value = "SELECT * FROM pago_empresa_centro p WHERE e.empresa_mail_pago = :mailEmpresa && e.centro_mail_pago = :mailCentro", nativeQuery = true)
+    Pago findOneByEmpresaAndCentro(@Param("mailEmpresa") String mailEmpresa,@Param("mailEmpresa") String mailCentro);
 }
---
+++ src/main/java/gym/gymServer/Security/Configuracion/PagosConfiguration.java
@@ -0,0 +1,59 @@
+package gym.gymServer.Security.Configuracion;
+
+import gym.gymServer.Classes.*;
+import gym.gymServer.Repository.EmpleadoRepository;
+import gym.gymServer.Repository.InscripcionesActividadesRepository;
+import gym.gymServer.Repository.PagoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.transaction.Transactional;
+import java.time.temporal.Temporal;
+import java.util.Date;
+import java.util.List;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
+public class PagosConfiguration {
+
+    @Autowired
+    PagoRepository pagoRepository;
+
+    @Autowired
+    InscripcionesActividadesRepository inscripcionesActividadesRepository;
+
+    @Transactional
+    public void cobrar(){
+        List<InscripcionesActividades> inscripcionesActividades = (List<InscripcionesActividades>) inscripcionesActividadesRepository.findAll();
+        for (int i = 0; i < inscripcionesActividades.size(); i++) {
+            InscripcionesActividades inscripcionActividad_i = inscripcionesActividades.get(i);
+
+            Empresas empresa_i=inscripcionActividad_i.getEmpleado().getEmpresa();
+            CentrosDeportivos centro_deportivo_i=inscripcionActividad_i.getActividad().getCentroDeportivo();
+            Actividad actividad_i=inscripcionActividad_i.getActividad();
+
+            long daysBetween=DAYS.between((Temporal) new Date(),actividad_i.getDia());
+
+            if(((inscripcionActividad_i.getTipoReserva().equals("RESERVAR") &&  daysBetween<=0) || inscripcionActividad_i.isAsistencia()) && !inscripcionActividad_i.equals("PAGO")){
+                Integer monto_a_cobrar_i=actividad_i.getCosto();
+                Pago pago_empresa_centro_i=pagoRepository.findOneByEmpresaAndCentro(empresa_i.getMail(),centro_deportivo_i.getMail());
+                if(pago_empresa_centro_i!=null){
+                    pago_empresa_centro_i.setMonto(pago_empresa_centro_i.getMonto()+monto_a_cobrar_i);
+                }
+                else{
+                    pago_empresa_centro_i=new Pago(empresa_i.getMail(),centro_deportivo_i.getMail(),monto_a_cobrar_i);
+                }
+                inscripcionActividad_i.setTipoReserva("PAGO");
+            }
+        }
+
+    }
+
+
+
+}
